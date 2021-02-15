e53cb96667d00067cd63aa9bdd9e7b651d341b7c
Jeff Martin <jeff@reportmill.com>
2021-02-15 16:05:22 -0600

    RMImageShape: Fix unusual legacy case of RMImageShape with PDF image fill.

--- src/rmdraw/scene/SGImage.java
+++ src/rmdraw/scene/SGImage.java
@@ -252,11 +252,10 @@ public class SGImage extends SGRect {
         // Unarchive basic attributes
         super.fromXML(anArchiver, anElement);
 
-        // Unarchive Image resource: get resource bytes and set Image (if PDF - swap out RMPDFShape)
+        // Unarchive Image resource: get resource bytes and set Image
         String rname = anElement.getAttributeValue("resource");
         if (rname!=null) {
             byte bytes[] = anArchiver.getResource(rname);
-            //if(RMPDFData.canRead(bytes)) return new RMPDFShape().fromXML(anArchiver, anElement);
             _imgRef = ImageRef.getImageRef(bytes);
         }
 
@@ -273,33 +272,6 @@ public class SGImage extends SGRect {
         if (anElement.hasAttribute("Padding")) setPadding(anElement.getAttributeIntValue("Padding"));
         if (anElement.hasAttribute("GrowToFit")) setGrowToFit(anElement.getAttributeBooleanValue("GrowToFit"));
         if (anElement.hasAttribute("PreserveRatio")) setPreserveRatio(anElement.getAttributeBooleanValue("PreserveRatio"));
-
-        // Legacy alignment (RM14)
-        if (anElement.hasAttribute("Alignment")) {
-            String as = anElement.getAttributeValue("Alignment");
-            String s[] = { "TopLeft", "TopCenter", "TopRight", "CenterLeft", "Center", "CenterRight",
-                "BottomLeft", "BottomCenter", "BottomRight" };
-            int i = ArrayUtils.indexOf(s, as);
-            if (i>=0) setAlign(Pos.values()[i]);
-        }
-
-        // Legacy: If Fill is ImageFill and no ImageRef+Key or ImageFill.ImageRef, set ImageRef from IFill and clear fill
-        if (getFill() instanceof ImagePaint) { ImagePaint ifill = (ImagePaint)getFill();
-            XMLElement fill = anElement.get("fill");
-            ImageRef iref = ImageRef.getImageRef(ifill.getImage()); //ifill.getImageRef();
-            if (getImageRef()==null && !ifill.isTiled()) { // && getKey()==null) {
-                int fs = fill.getAttributeIntValue("fillstyle", 0); // Stretch=0, Tile=1, Fit=2, FitIfNeeded=3
-                if (fs==0) { setImageRef(iref); setFill(null); setGrowToFit(true); setPreserveRatio(false); }
-                else if (fs==2) { setImageRef(iref); setFill(null); setGrowToFit(true); setPreserveRatio(true); }
-                else if (fs==3) { setImageRef(iref); setFill(null); setGrowToFit(false); setPreserveRatio(true); }
-                double x = fill.getAttributeFloatValue("x");
-                double y = fill.getAttributeFloatValue("y");
-                if (x!=0) setAlignX(x<0 ? HPos.LEFT : HPos.RIGHT);
-                if (y!=0) setAlignY(y<0 ? VPos.TOP : VPos.BOTTOM);
-            }
-            else if (iref==null) setFill(null);
-            setPadding(fill.getAttributeIntValue("margin"));
-        }
 
         // Return
         return this;
