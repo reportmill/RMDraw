c054277ab90fc35598f7ec302ef049c339c2c22d
Jeff Martin <jeff@reportmill.com>
2023-04-17 08:32:58 -0500

    PolygonShape: Moved old path archival here from Path

--- src/rmdraw/scene/SGPolygon.java
+++ src/rmdraw/scene/SGPolygon.java
@@ -2,10 +2,7 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package rmdraw.scene;
-import snap.geom.Path;
-import snap.geom.Rect;
-import snap.geom.Shape;
-import snap.geom.Transform;
+import snap.geom.*;
 import snap.util.*;
 
 /**
@@ -73,9 +70,13 @@ public class SGPolygon extends SGParent {
      */
     protected XMLElement toXMLView(XMLArchiver anArchiver)
     {
-        XMLElement e = super.toXMLView(anArchiver);
-        e.setName("polygon");
-        e.add(_path.toXML(anArchiver));
+        XMLElement e = super.toXMLView(anArchiver); e.setName("polygon");
+
+        // Archive path
+        XMLElement pathXML = getXmlForPath(_path);
+        e.add(pathXML);
+
+        // Return
         return e;
     }
 
@@ -86,7 +87,7 @@ public class SGPolygon extends SGParent {
     {
         super.fromXMLView(anArchiver, anElement);
         XMLElement pathXML = anElement.get("path");
-        _path = anArchiver.fromXML(pathXML, Path.class, this);
+        _path = getPathFromXML(pathXML);
     }
 
     /**
@@ -97,5 +98,108 @@ public class SGPolygon extends SGParent {
         SGPolygon clone = (SGPolygon) super.clone();
         if (_path != null) clone._path = _path.clone();
         return clone;
+    }
+
+
+    /**
+     * XML archival for path.
+     */
+    private XMLElement getXmlForPath(Path2D aPath)
+    {
+        // Get new element named path
+        XMLElement e = new XMLElement("path");
+
+        // Archive individual elements/points
+        PathIter pathIter = aPath.getPathIter(null);
+        double[] points = new double[6];
+        while (pathIter.hasNext()) switch (pathIter.getNext(points)) {
+
+            // Handle MoveTo
+            case MoveTo:
+                XMLElement move = new XMLElement("mv");
+                move.add("x", points[0]);
+                move.add("y", points[1]);
+                e.add(move);
+                break;
+
+            // Handle LineTo
+            case LineTo:
+                XMLElement line = new XMLElement("ln");
+                line.add("x", points[0]);
+                line.add("y", points[1]);
+                e.add(line);
+                break;
+
+            // Handle QuadTo
+            case QuadTo:
+                XMLElement quad = new XMLElement("qd");
+                quad.add("cx", points[0]);
+                quad.add("cy", points[1]);
+                quad.add("x", points[2]);
+                quad.add("y", points[3]);
+                e.add(quad);
+                break;
+
+            // Handle CubicTo
+            case CubicTo:
+                XMLElement curve = new XMLElement("cv");
+                curve.add("cp1x", points[0]);
+                curve.add("cp1y", points[1]);
+                curve.add("cp2x", points[2]);
+                curve.add("cp2y", points[3]);
+                curve.add("x", points[4]);
+                curve.add("y", points[5]);
+                e.add(curve);
+                break;
+
+            // Handle Close
+            case Close:
+                XMLElement close = new XMLElement("cl");
+                e.add(close);
+                break;
+        }
+
+        return e;
+    }
+
+    /**
+     * XML unarchival for path.
+     */
+    public Path getPathFromXML(XMLElement anElement)
+    {
+        Path path = new Path();
+
+        // Unarchive individual elements/points
+        for (int i = 0, iMax = anElement.size(); i < iMax; i++) {
+            XMLElement segXML = anElement.get(i);
+            String segName = segXML.getName();
+            double endX = segXML.getAttributeFloatValue("x");
+            double endY = segXML.getAttributeFloatValue("y");
+
+            switch (segName) {
+                case "mv": path.moveTo(endX, endY); break;
+                case "ln":path.lineTo(endX, endY); break;
+                case "qd": {
+                    double cpx = segXML.getAttributeFloatValue("cx");
+                    double cpy = segXML.getAttributeFloatValue("cy");
+                    path.quadTo(cpx, cpy, endX, endY);
+                    break;
+                }
+                case "cv": {
+                    double cp1x = segXML.getAttributeFloatValue("cp1x");
+                    double cp1y = segXML.getAttributeFloatValue("cp1y");
+                    double cp2x = segXML.getAttributeFloatValue("cp2x");
+                    double cp2y = segXML.getAttributeFloatValue("cp2y");
+                    path.curveTo(cp1x, cp1y, cp2x, cp2y, endX, endY);
+                    break;
+                }
+                case "cl":
+                    path.close();
+                    break;
+            }
+        }
+
+        // Return
+        return path;
     }
 }
